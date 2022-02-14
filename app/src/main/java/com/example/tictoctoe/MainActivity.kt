ccf85519c82bd225a9517e33363fedebf203c69d
zahhard <za.darardoust@gmail.com>
2022-02-14 20:46:09 +0330

    i add a fun called reset to reset game. i add a fun called stopTouch to stop game

--- app/src/main/java/com/example/tictoctoe/MainActivity.kt
+++ app/src/main/java/com/example/tictoctoe/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.tictoctoe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.example.tictoctoe.databinding.ActivityMainBinding
+import java.util.*
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val view = binding.root
+        setContentView(view)
     }
+
+    fun reset(){
+        binding.button2.text = ""
+        binding.button3.text = ""
+        binding.button4.text = ""
+        binding.button5.text = ""
+        binding.button6.text = ""
+        binding.button7.text = ""
+        binding.button8.text = ""
+        binding.button9.text = ""
+        binding.button10.text = ""
+
+        binding.textView3Winner.text = "Click to start"
+    }
+
+    fun stopTouch()
+    {
+        binding.button2.isEnabled = false
+        binding.button2.isEnabled = false
+        binding.button3.isEnabled = false
+        binding.button4.isEnabled = false
+        binding.button5.isEnabled = false
+        binding.button6.isEnabled = false
+        binding.button7.isEnabled = false
+        binding.button8.isEnabled = false
+        binding.button9.isEnabled = false
+    }
+
 }
